4b650e4340433cf108759ab2645f88668d763672
boterop <boterop22@gmail.com>
2025-06-21 19:58:17 -0500

    Main game

--- scripts/bingo.js
+++ scripts/bingo.js
@@ -1,5 +1,18 @@
-const current = 0;
+const CELL_STYLE =
+  "height: 60px; font-size: 2rem; font-weight: bold; text-align: center; padding: 10px; align-items: center; display: flex; justify-content: center;";
+
+let current = 0;
+let timer = null;
 const balls = [];
+
+const getColor = (number) => {
+  const color = balls.includes(number)
+    ? "background-color: red; color: white;"
+    : number === current
+      ? "background-color: yellow; color: red;"
+      : "background-color: white; color: black;";
+  return color;
+};
 
 const createGrid = () => {
   for (let i = 0; i < 5; i++) {
@@ -7,14 +20,10 @@ const createGrid = () => {
     for (let j = 0; j < 15; j++) {
       const cell = document.createElement("div");
       const number = j + 1 + 15 * i;
-      const color = balls.includes(number)
-        ? "background-color: red; color: white;"
-        : number === current
-          ? "background-color: yellow; color: red;"
-          : "background-color: white; color: black;";
+      const color = getColor(number);
 
       cell.id = `cell-${number}`;
-      cell.style = `height: 60px; font-size: 2rem; font-weight: bold; text-align: center; padding: 10px; align-items: center; display: flex; justify-content: center; ${color}`;
+      cell.style = `${CELL_STYLE} ${color}`;
       cell.textContent = number;
 
       row.appendChild(cell);
@@ -22,4 +31,55 @@ const createGrid = () => {
   }
 };
 
+const getNext = () => {
+  const next = Math.floor(Math.random() * 75) + 1;
+
+  if (balls.includes(next)) return getNext();
+
+  return next;
+};
+
+const play = (file) => {
+  const audio = new Audio(`assets/audio/${file}`);
+  audio.play().catch((error) => {
+    console.error(`Error playing audio ${file}`, error);
+  });
+};
+
+const say = (number) => play(`/numbers/hombre/${number}.wav`);
+
+const updateNumber = (number) => {
+  const cell = document.getElementById(`cell-${number}`);
+  if (cell) {
+    const color = getColor(number);
+    cell.style = `${CELL_STYLE} ${color}`;
+  }
+};
+
 createGrid();
+
+document.addEventListener("keypress", (event) => {
+  const key = event.key.toLowerCase();
+  switch (key) {
+    case " ":
+      play("tono.wav");
+      if (!timer) {
+        timer = setInterval(() => {
+          const next = getNext();
+
+          const latest = current;
+          balls.push(latest);
+          current = next;
+          say(next);
+          updateNumber(latest);
+          updateNumber(next);
+        }, 6 * 1000);
+      } else {
+        clearInterval(timer);
+        timer = null;
+      }
+      break;
+    default:
+      break;
+  }
+});
